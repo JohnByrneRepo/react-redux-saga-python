16ac04728e8d823a2c395f5b2ad9b1d161ac5877
John Byrne <html5css3@outlook.com>
2018-07-18 09:16:25 +0100

    Update build

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,12 +7,7 @@ export default class Routes extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/details/:id" component={DetailsPage} />
-          </Switch>
-        </BrowserRouter>
+        <HomePage />
       </div>
     )
   }
